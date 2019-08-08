533688c9d27cf24c5594482cd85a2b0eb492073c
DavisOsiemo <davisossy@gmail.com>
2019-08-09 01:32:59 +0300

    kesho

---
+++ src/main/scala/main2.scala
@@ -0,0 +1,21 @@
+import com.uberBlacklist.web.service.WebServer
+import akka.http.scaladsl.server._
+
+object main2 {
+  def main(args: Array[String]): Unit = {
+    /*implicit val system           = ActorSystem()
+    implicit val materializer     = ActorMaterializer()
+    implicit val executionContext = system.dispatcher*/
+
+    val port: Int = sys.env.getOrElse("PORT", "8080").toInt
+    WebServer.startServer("0.0.0.0", port)
+  }
+}
+object WebServer extends HttpApp {
+  override protected def routes: Route =
+    pathEndOrSingleSlash {
+      get {
+        complete("It works")
+      }
+    }
+}
